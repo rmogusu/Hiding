9745ad05575011e8b7d8a9ad097bd68f70f44813
mrsfreddy <rmogusu123@gmal.com>
2019-07-17 09:05:17 -0400

    Updated my files

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
         $(".clickable").click(function() {
           $("#image-showing").toggle();
           $("#image-hidden").toggle();
+
         });
       });
+$(document).ready(function() {
+              $(".clickable").click(function() {
+
+                $("#giant-showing").toggle();
+                $("#giant-hidden").toggle();
+      });
+  });
